0a3cb78fa1e03a6549617d8e45c0503d7f0176a8
drifter089 <akshatmittal.0007@gmail.com>
2024-07-29 04:08:23 +0700

    postgress with data backup

---
+++ geoserver/data_dir/workspaces/PyPSAEarthDashboard/styles/Buses_geojson_data_Style.sld
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<StyledLayerDescriptor xmlns="http://www.opengis.net/sld" xsi:schemaLocation="http://www.opengis.net/sld http://schemas.opengis.net/sld/1.1.0/StyledLayerDescriptor.xsd" xmlns:se="http://www.opengis.net/se" version="1.1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <NamedLayer>
+    <se:Name>Buses_geojson_data</se:Name>
+    <UserStyle>
+      <se:Name>Buses_geojson_data</se:Name>
+      <se:FeatureTypeStyle>
+        <se:Rule>
+          <se:Name>Single symbol</se:Name>
+          <se:PointSymbolizer>
+            <se:Graphic>
+              <se:Mark>
+                <se:WellKnownName>circle</se:WellKnownName>
+                <se:Fill>
+                  <se:SvgParameter name="fill">#487bb6</se:SvgParameter>
+                </se:Fill>
+                <se:Stroke>
+                  <se:SvgParameter name="stroke">#325780</se:SvgParameter>
+                  <se:SvgParameter name="stroke-width">1</se:SvgParameter>
+                </se:Stroke>
+              </se:Mark>
+              <se:Size>18</se:Size>
+            </se:Graphic>
+          </se:PointSymbolizer>
+        </se:Rule>
+      </se:FeatureTypeStyle>
+    </UserStyle>
+  </NamedLayer>
+</StyledLayerDescriptor>
---
+++ postgres/entrypoint.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+
+# Initialize the PostgreSQL data directory
+initdb -D ${PGDATA}
+
+# Copy custom configuration files to the PostgreSQL data directory
+cp /data/postgresql.conf ${PGDATA}/postgresql.conf
+cp /data/pg_hba.conf ${PGDATA}/pg_hba.conf
+
+# Start PostgreSQL in the background with the custom configuration file
+pg_ctl -D ${PGDATA} -l /var/log/postgresql/logfile start
+
+# Wait for PostgreSQL to start
+wait_postgresql() {
+  while ! pg_isready -q; do
+    echo "Waiting for PostgreSQL to start..."
+    sleep 1
+  done
+}
+wait_postgresql
+
+
+createdb template_postgis
+
+psql -d template_postgis -c "CREATE EXTENSION postgis;"
+psql -d template_postgis -c "CREATE EXTENSION postgis_topology;"
+
+# Create the Postgres database named "mypostgresDB" with the "template_postgis" template
+createdb mypostgresDB --template=template_postgis
+
+# Extract the schema and data from the backup file
+pg_restore -f /tmp/schema_new.sql -s /data/mypostgresDB-backup.sql 2>&1 | tee /var/log/postgresql/schema_extract.log
+pg_restore -f /tmp/new_data.sql -a /data/mypostgresDB-backup.sql 2>&1 | tee /var/log/postgresql/data_extract.log
+
+# Import the schema and data into the new database
+psql --dbname=mypostgresDB -f /tmp/schema_new.sql 2>&1 | tee /var/log/postgresql/schema_import.log
+psql --dbname=mypostgresDB -f /tmp/new_data.sql 2>&1 | tee /var/log/postgresql/data_import.log
+
+# # Stop the PostgreSQL server cleanly
+pg_ctl -D ${PGDATA} -m fast -w stop
+
+# Start PostgreSQL in the foreground with the custom configuration file
+# exec postgres -D ${PGDATA} -c config_file=${PGDATA}/postgresql.conf -c logging_collector=on -c log_directory='/var/log/postgresql' -c log_filename='logfile'
+
+pg_ctl -D ${PGDATA} -l /var/log/postgresql/logfile start
+
+wait_postgresql
+
+tail -f /var/log/postgresql/logfile
