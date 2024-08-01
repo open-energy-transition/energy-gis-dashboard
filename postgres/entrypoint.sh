3a3959dec15f4cf66d970bc93db0cfd6fd3e2562
drifter089 <akshatmittal.0007@gmail.com>
2024-08-01 18:11:18 +0700

    final local version

--- postgres/entrypoint.sh
+++ postgres/entrypoint.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 set -e
 
-
 export PGPASSWORD=${POSTGRES_PASSWORD}
 
 # Initialize the PostgreSQL data directory with md5 authentication
-initdb -D ${PGDATA} -A md5 --pwfile=<(echo "$POSTGRES_PASSWORD")
+initdb -D ${PGDATA} -A md5 --pwfile=<(echo "$PGPASSWORD")
 
-# Initialize the PostgreSQL data directory
-# initdb -D ${PGDATA} -A md5
-
-# Copy custom configuration files to the PostgreSQL data directory
-cp /data/postgresql.conf ${PGDATA}/postgresql.conf
-cp /data/pg_hba.conf ${PGDATA}/pg_hba.conf
+echo "host all all 172.17.0.0/16 md5" >> ${PGDATA}/pg_hba.conf
 
 # Start PostgreSQL in the background with the custom configuration file
 pg_ctl -D ${PGDATA} -l /var/log/postgresql/logfile start
@@ -26,28 +20,19 @@ wait_postgresql() {
 }
 wait_postgresql
 
-
-createdb template_postgis
-
-psql -d template_postgis -c "CREATE EXTENSION postgis;"
-psql -d template_postgis -c "CREATE EXTENSION postgis_topology;"
-
-# Create the Postgres database named "mypostgresDB" with the "template_postgis" template
-createdb mypostgresDB --template=template_postgis
+createdb ${POSTGRES_DB} --template=template1
 
 # Extract the schema and data from the backup file
 pg_restore -f /tmp/schema_new.sql -s /data/mypostgresDB-backup.sql 2>&1 | tee /var/log/postgresql/schema_extract.log
 pg_restore -f /tmp/new_data.sql -a /data/mypostgresDB-backup.sql 2>&1 | tee /var/log/postgresql/data_extract.log
 
 # Import the schema and data into the new database
-psql --dbname=mypostgresDB -f /tmp/schema_new.sql 2>&1 | tee /var/log/postgresql/schema_import.log
-psql --dbname=mypostgresDB -f /tmp/new_data.sql 2>&1 | tee /var/log/postgresql/data_import.log
+psql --dbname=${POSTGRES_DB} -f /tmp/schema_new.sql 2>&1 | tee /var/log/postgresql/schema_import.log
+psql --dbname=${POSTGRES_DB} -f /tmp/new_data.sql 2>&1 | tee /var/log/postgresql/data_import.log
 
-# # Stop the PostgreSQL server cleanly
+
+# Stop the PostgreSQL server cleanly
 pg_ctl -D ${PGDATA} -m fast -w stop
-
-# Start PostgreSQL in the foreground with the custom configuration file
-# exec postgres -D ${PGDATA} -c config_file=${PGDATA}/postgresql.conf -c logging_collector=on -c log_directory='/var/log/postgresql' -c log_filename='logfile'
 
 pg_ctl -D ${PGDATA} -l /var/log/postgresql/logfile start
 
